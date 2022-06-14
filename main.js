5cf17d4be4247b94913dc5824ad47ded530128bf
JacksonBarbosa <92748701+JacksonBarbosa@users.noreply.github.com>
2022-06-14 14:36:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+  ]
+
+    //clicar em fazer pergunta
+function fazerPergunta() {
+
+  const elementoresposta = document.getElementById("resposta")
+  const inputPergunta = document.getElementById("inputPergunta")
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+  }
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+    //gerar numero aleatorio
+  
+    const respondendo = document.getElementById("b1")
+    const totalRespostas = respostas.length
+    const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+    elementoresposta.innerHTML = pergunta + respostas [numeroAleatorio]
+
+    // sumir com a resposta depois de um tempo
+
+    setTimeout(function() {
+      elementoresposta.style.opacity = 0;
+    }, 3000)
+}
